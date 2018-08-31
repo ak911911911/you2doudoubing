12c283cc9f552e46a358dd513c7723adf26e86ca
ak911911911 <42665073+ak911911911@users.noreply.github.com>
2018-08-31 10:23:04 +0300

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,7 +2,7 @@
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
 define('GJ_CODE', 'JP');
-define('SITE_NAME', '大爷常来玩儿啊');
+define('SITE_NAME', '河殇');
 define('TITLENAME', 'DouDouTube');
 define('EN2DEKEY', 'lujianbupingyishenghou');
 define('EMAIL', 'xiaofufua@protonmail.com');
